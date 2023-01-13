013b8e9e343965acf7d7c2d63540038abc7ed4f3
rusconn <rusconn3@gmail.com>
2023-01-13 16:36:52 +0000

    Improve error messages

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::path::PathBuf;
 
+use anyhow::Context;
 use structopt::StructOpt;
 
 use cli::{Action::*, CommandLineArgs};
@@ -9,7 +10,7 @@ mod cli;
 mod file;
 mod tasks;
 
-fn main() {
+fn main() -> anyhow::Result<()> {
     let CommandLineArgs {
         action,
         journal_file,
@@ -17,14 +18,15 @@ fn main() {
 
     let journal_file = journal_file
         .or_else(find_default_journal_file)
-        .expect("Failed to find journal file");
+        .context("Failed to find journal file")?;
 
     match action {
         Add { text } => tasks::add_task(journal_file, Task::new(text)),
         List => tasks::list_tasks(journal_file),
         Done { position } => tasks::complete_task(journal_file, position),
-    }
-    .expect("Failed to perform action")
+    }?;
+
+    Ok(())
 }
 
 fn find_default_journal_file() -> Option<PathBuf> {
