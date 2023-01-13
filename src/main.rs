b20f59ad3c9e2947ea9bbe0a43e2519194022e90
rusconn <rusconn3@gmail.com>
2023-01-13 15:54:52 +0000

    Implement main

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,24 @@
 use structopt::StructOpt;
 
+use cli::{Action::*, CommandLineArgs};
+use tasks::Task;
+
 mod cli;
 mod file;
 mod tasks;
 
 fn main() {
-    let args = cli::CommandLineArgs::from_args();
+    let CommandLineArgs {
+        action,
+        journal_file,
+    } = CommandLineArgs::from_args();
 
-    println!("{:#?}", args);
+    let journal_file = journal_file.expect("Failed to find journal file");
+
+    match action {
+        Add { text } => tasks::add_task(journal_file, Task::new(text)),
+        List => tasks::list_tasks(journal_file),
+        Done { position } => tasks::complete_task(journal_file, position),
+    }
+    .expect("Failed to perform action")
 }
